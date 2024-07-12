de4c307503f0efb51daa651bd3e6bb7d149d8673
Evandro2270 <evandro535@gmail.com>
2024-07-12 17:19:59 -0300

    Adicionar arquivos de projeto.

---
+++ URI 1038/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                string[] vet = Console.ReadLine().Split(' ');
+                int codigo = int.Parse(vet[0]);
+                int qte = int.Parse(vet[1]);
+
+                double total;
+                
+                if (codigo == 1)
+                {
+                    total = qte * 4.0;
+                }
+                else if (codigo == 2)
+                {
+                    total = qte * 4.5;
+                }
+                else if (codigo == 2)
+                {
+                    total = qte * 4.5;
+                }
+                else if (codigo == 3)
+                {
+                    total = qte * 5.0;
+                }
+                else if (codigo == 4)
+                {
+                    total = qte * 2.0;
+                }
+                else
+                {
+                    total = qte * 1.5;
+                }
+                Console.WriteLine("Total: R$ " + total.ToString("F2, CI"));
+            }
+        }
+    }
+}
